5381d6a90393ee0514255cfafa8a4ababcf63ffd
Fernando Zárate <150300719+fernandozarate07@users.noreply.github.com>
2024-09-14 21:33:43 -0300

    Add variables for DOM elements

---
+++ app.js
@@ -0,0 +1,17 @@
+const btnCreate = document.querySelector('#buttomControlDiv .dccButtonCreate');
+const btnClear = document.querySelector('#buttomControlDiv .dccButtonClear');
+
+const btnColorBlack = document.querySelector('.colorBlack');
+const btnColorWhite = document.querySelector('.colorWhite');
+const btnColorRed = document.querySelector('.colorRed');
+const btnColorBlue = document.querySelector('.colorBlue');
+const btnColorGreen = document.querySelector('.colorGreen');
+const btnColorYellow = document.querySelector('.colorYellow');
+const btnColorAqua = document.querySelector('.colorAqua');
+const btnColorMagenta = document.querySelector('.colorMagenta');
+const btnColorOrange = document.querySelector('.colorOrange');
+const btnColorGrey = document.querySelector('.colorGrey');
+const btnColorRainbow = document.querySelector('.colorRainbow');
+const btnEraser = document.querySelector('.eraser');
+
+const inputValue = document.querySelector('.inputRangeDiv_value');
